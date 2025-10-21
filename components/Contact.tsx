69d0732ff0cf27a1cd57c99e5f8dcd37c0948324
Mohamed Ghareeb <mohamedghareeb20@yahoo.com>
2025-10-21 03:17:51 +0200

    four commit

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MdOutlineMail , MdOutlineLocalPhone } from "react-icons/md";
 import { FaLinkedinIn } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa6";
 
 
 import Link from 'next/link';
@@ -9,24 +10,30 @@ export default function Contact() {
   return (
     <div className='px-4 md:px-8 py-8 mt-5' id='contact'>
         <h2 className='text-center font-bold text-[32px] md:text-[48px]'>Contact Me</h2>
-        <div className="mt-8 w-full lg:max-w-[80%] mx-auto p-4 border-[1] border-black rounded-md flex flex-col md:flex-row items-start md:items-center justify-center gap-4">
-            <Link href={"mailto:ghareebmohamed406@gmail.com"} className='w-full flex md:justify-center items-center gap-2'>
+        <div className="mt-8 w-full p-4 border-[1] border-black rounded-md grid grid-cols-4 md:grid-cols-2 gap-4">
+            <Link href={"mailto:ghareebmohamed406@gmail.com"} className='w-full flex items-center gap-2'>
                 <div className='w-10 h-10 rounded-full bg-black flex items-center justify-center'>
                     <MdOutlineMail className='text-2xl text-white'/>
                 </div>
-                <div className='text-base md:text-lg font-semibold line-clamp-1'>ghareebmohamed406@gmail.com</div>
+                <div className='hidden md:block text-base font-semibold'>ghareebmohamed406@gmail.com</div>
             </Link>
-            <Link href={"https://www.linkedin.com/in/mohamed-ghareeb-0ab082334/"} target='blank' className='w-full flex md:justify-center items-center gap-2'>
+            <Link href={"https://www.linkedin.com/in/mohamed-ghareeb-0ab082334/"} target='blank' className='w-full flex items-center gap-2'>
                 <div className='w-10 h-10 rounded-full bg-black flex items-center justify-center'>
                     <FaLinkedinIn className='text-2xl text-white'/>
                 </div>
-                <div className='text-base md:text-lg font-semibold'>LinkedIn</div>
+                <div className='hidden md:block text-base font-semibold'>LinkedIn</div>
             </Link>
-            <Link href={"tel:01140872874"} className='w-full flex md:justify-center items-center gap-2'>
+            <Link href={"https://wa.me/201140872874"} target='blank' className='w-full flex items-center gap-2'>
+                <div className='w-10 h-10 rounded-full bg-black flex items-center justify-center'>
+                    <FaWhatsapp className='text-2xl text-white'/>
+                </div>
+                <div className='hidden md:block text-base font-semibold'>Whatsapp</div>
+            </Link>
+            <Link href={"tel:01140872874"} className='w-full flex items-center gap-2'>
                 <div className='w-10 h-10 rounded-full bg-black flex items-center justify-center'>
                     <MdOutlineLocalPhone className='text-2xl text-white'/>
                 </div>
-                <div className='text-base md:text-lg font-semibold'>01140872874</div>
+                <div className='hidden md:block text-base font-semibold'>01140872874</div>
             </Link>
         </div>
     </div>
